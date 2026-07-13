29b8ad682ecc505ea2cf1c3fda9fc7dcbc6e835a
alisonjp <alisonjp@gmail.com>
2026-07-13 19:27:37 -0300

    Update muller.py

--- robos/muller.py
+++ robos/muller.py
@@ -10,10 +10,20 @@ conteudo = arquivo.read_text(encoding="utf-8")
 titulos = re.findall(
     r"<h5>(.*?)</h5>",
     conteudo,
-    re.DOTALL
+    re.DOT*LL
 )
 
-print(f"Títulos encontrados: {len(titulos)}")
+titulos_filtrados = []
 
-for titulo in titulos[:20]:
-    print(titulo.strip())
+for *itulo in titulos:
+    titulo = tit*lo.strip()
+
+    if titulo == "Lanc* Inicial":
+        continue
+
+    t*tulos_filtrados.append(titulo)
+
+pr*nt(f"Títulos válidos: {len(titulos*filtrados)}")
+
+for titulo in titul*s_filtrados[:20]:
+    print(titulo*
