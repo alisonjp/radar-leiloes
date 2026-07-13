09688188ef1328e353a278ebf365f24e0b7bae15
alisonjp <alisonjp@gmail.com>
2026-07-13 19:05:06 -0300

    Update muller.py

--- robos/muller.py
+++ robos/muller.py
@@ -1,1 +1,10 @@
+from pathlib import Path
+
+arquivo = Path("amostra_muller.html")
+
 print("Robô Müller iniciado")
+
+if arquivo.exists():
+    print("Arquivo encontrado")
+else:
+    print("Arquivo não encontrado")
